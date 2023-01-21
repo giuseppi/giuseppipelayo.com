8b617a00b3fd7de34ac934c47c6c0f6669368935
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-01-21 15:49:18 -0800

    created intro.js
    - will include intro box
    - name, job title/aspiration
    - button linking to about me

---
+++ src/intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div id="intro-container">
+      <h1 id="name">Giuseppi Pelayo</h1>
+    </div>
+  );
+};
+
+export default Intro;
