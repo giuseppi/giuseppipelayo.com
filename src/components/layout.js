419cd4326c3ea717cae076ee09b178acfcb506dc
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-01-24 15:55:50 -0800

    added footer.js, working on flexbox elements

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import Header from "./header.js";
 import Projects from "./projects.js";
+import Footer from "./footer.js";
 const Layout = () => {
   return (
     <>
       <div className="flex-container">
         <Header />
         <Projects />
+        <Footer />
       </div>
     </>
   );
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <section className="projects-section" id="projects">
+    <div className="projects-container" id="projects">
       <div className="project-content">
         <div className="projects-heading-container">
           <h4 className="projects-heading">Projects</h4>
@@ -11,7 +11,7 @@ const Projects = () => {
           <h2 className="projects-description">See what I've been working on.</h2>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
