4e70580f16e51fac383b2b21e7204fc5717b039d
Giuseppi Pelayo <giuseppipelayo@gmail.com>
2023-01-26 12:31:51 -0800

    added about title + description

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,16 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="about-container">hello</div>
+      <div className="about-container">
+        <div className="about-content">
+          <div className="about-heading-container">
+            <h1 className="about-heading">About Me</h1>
+          </div>
+          <div className="about-description-container">
+            <h4 className="about-description">Get to know me!</h4>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
