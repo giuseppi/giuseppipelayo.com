6a69ffcd8680c90ca08ab93ccf41c51c4c6c36e3
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-01-25 20:48:06 -0800

    changed header socials to UL to keep it vertical
    prroblem was it would change to horizontal everytime i started the application

--- src/components/header.js
+++ src/components/header.js
@@ -12,20 +12,24 @@ const Header = () => {
         <Navbar />
         <h1 className="name">Hi, I'm Giuseppi</h1>
         <h1 className="slogan">Aspiring Developer</h1>
-        <div className="header-content">
-          <a className="headerlink" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/giuseppi-pelayo-a971b515a">
-            <FontAwesomeIcon icon={faLinkedinIn} className="header-icon" id="head-li" />
-            <p className="header-description" id="header-linkedin">
-              Linkedin
-            </p>
-          </a>
-          <a className="headerlink" target="_blank" rel="noreferrer" href="https://github.com/giuseppi">
-            <FontAwesomeIcon icon={faGithub} className="header-icon" id="head-gh" />
-            <p className="header-description" id="header-github">
-              Github
-            </p>
-          </a>
-        </div>
+        <ul className="header-content">
+          <li>
+            <a className="headerlink" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/giuseppi-pelayo-a971b515a">
+              <FontAwesomeIcon icon={faLinkedinIn} className="header-icon" id="head-li" />
+              <p className="header-description" id="header-linkedin">
+                Linkedin
+              </p>
+            </a>
+          </li>
+          <li>
+            <a className="headerlink" target="_blank" rel="noreferrer" href="https://github.com/giuseppi">
+              <FontAwesomeIcon icon={faGithub} className="header-icon" id="head-gh" />
+              <p className="header-description" id="header-github">
+                Github
+              </p>
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   );
