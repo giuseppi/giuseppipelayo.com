89771fbae09355fe67d54ca937ee082aeead7922
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-02-03 15:12:58 -0800

    introduced animated popup close button

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavItem from "./navitem.js";
+
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Popup = (props) => {
   return props.trigger ? (
@@ -6,7 +9,9 @@ const Popup = (props) => {
       <div className="popup-container">
         <div className="popup-text">test</div>
         <div className="popup-form">
-          <button className="close-btn" onClick={() => props.setTrigger(false)}></button>
+          <button className="close-btn" onClick={() => props.setTrigger(false)}>
+            <NavItem iconClass={"popup"} descClass={"popup"} descID={"popup-close"} icon={faXmark} description="close" />
+          </button>
           {props.children}
         </div>
       </div>
