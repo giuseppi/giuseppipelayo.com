8c30c9a031fdec513559323f4976f3e992dc16f0
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-02-06 13:38:22 -0800

    deleted unnecessary project div

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,11 +3,9 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="projects-container" id="projects">
-      <div className="project-content">
-        <div className="projects-heading-container">
-          <h1 className="projects-heading">Projects</h1>
-          <h4 className="projects-description">See what I've been working on.</h4>
-        </div>
+      <div className="projects-heading-container">
+        <h1 className="projects-heading">Projects</h1>
+        <h4 className="projects-description">See what I've been working on.</h4>
       </div>
     </div>
   );
