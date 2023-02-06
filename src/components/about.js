115f1251374be994dbdab4548c395f392818d8f1
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-02-06 13:37:33 -0800

    completed projects-arrow
    TODO: fix projects arrow as well when resizing

--- src/components/about.js
+++ src/components/about.js
@@ -1,27 +1,33 @@
 import React from "react";
+import { HashLink } from "react-router-hash-link";
 import Temp from "../img/temp_pic.jpg";
 
 const About = () => {
   return (
     <>
       <div className="about-container" id="about">
-        <div className="about-content">
-          <div className="about-heading-container">
-            <h1>About Me</h1>
-            <h4>Get to know me!</h4>
+        <div className="about-heading-container">
+          <h1>About Me</h1>
+          <h4>Get to know me!</h4>
+        </div>
+        <div className="about-me">
+          <div className="about-description">
+            <p>
+              Hello world! I am Giuseppi Pelayo, an aspiring web developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium iste
+              est molestiae odit, reprehenderit numquam magni sunt libero at recusandae necessitatibus iusto mollitia quibusdam omnis minus architecto
+              aspernatur suscipit. Beatae? Rerum asperiores ratione accusantium voluptates repellendus, harum perferendis provident
+            </p>
           </div>
-          <div className="about-me">
-            <div className="about-description">
-              <p>
-                Hello world! I am Giuseppi Pelayo, an aspiring web developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-                iste est molestiae odit, reprehenderit numquam magni sunt libero at recusandae necessitatibus iusto mollitia quibusdam omnis minus
-                architecto aspernatur suscipit. Beatae? Rerum asperiores ratione accusantium voluptates repellendus, harum perferendis provident
-              </p>
-            </div>
-            <div className="about-image">
-              <img src={Temp} alt="temp" />
-            </div>
+          <div className="about-image">
+            <img src={Temp} alt="temp" />
           </div>
+        </div>
+        <div className="arrow-projects">
+          <HashLink smooth to="#projects" class="arrow-link">
+            <div class="arrow"></div>
+            <div class="arrow"></div>
+            <div class="arrow"></div>
+          </HashLink>
         </div>
       </div>
     </>
