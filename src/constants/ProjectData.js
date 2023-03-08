1b9954161308f0cd3479c0f93a26fc00a7fa1ebc
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-03-08 13:56:58 -0800

    updated project brief descs

--- src/constants/ProjectData.js
+++ src/constants/ProjectData.js
@@ -2,7 +2,7 @@ export const properties = [
   {
     id: 1,
     project: "Portfolio",
-    brief: "this is Portfolio",
+    brief: "My first project created to display my front-end abilities and to display my work.",
     description: "Portfolio description",
     imgURL: "https://cdn.discordapp.com/attachments/323728336469622785/1083140425885425726/image.png",
     progress: {
@@ -13,7 +13,7 @@ export const properties = [
   {
     id: 2,
     project: "GFit",
-    brief: "this is GFit",
+    brief: "A web application that allows users to track their fitness progress seamlessly.",
     description: "GFit description",
     imgURL: "https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg?auto=compress&cs=tinysrgb&w=400",
     progress: {
