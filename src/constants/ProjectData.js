5b374894470c2954254aaf188a3ddb81e043395f
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-03-08 13:36:05 -0800

    added temp project images

--- src/constants/ProjectData.js
+++ src/constants/ProjectData.js
@@ -4,13 +4,13 @@ export const properties = [
     project: "Portfolio",
     brief: "this is Portfolio",
     description: "Portfolio description",
-    imgURL: "test",
+    imgURL: "https://cdn.discordapp.com/attachments/323728336469622785/1083140425885425726/image.png",
   },
   {
     id: 2,
     project: "GFit",
     brief: "this is GFit",
     description: "GFit description",
-    imgURL: "test2",
+    imgURL: "https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg?auto=compress&cs=tinysrgb&w=400",
   },
 ];
