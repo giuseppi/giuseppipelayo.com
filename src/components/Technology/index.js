c3767b46d3095f152770633264e565a6f87617be
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2023-03-08 23:35:54 -0800

    fixed better build

--- src/components/Technology/index.js
+++ src/components/Technology/index.js
@@ -7,7 +7,7 @@ const Technology = ({ iconName, iconLabel }) => {
   return (
     <div className="technology">
       <div className="technology_circle">
-        <FontAwesomeIcon className="technology_icon" icon="fa-brands fa-react" />
+        <FontAwesomeIcon className="technology_icon" icon={iconName} />
       </div>
       <span className="technology_label">{iconLabel}</span>
     </div>
